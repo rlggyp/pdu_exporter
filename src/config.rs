f4de9dfb4dfdfe942b7ab1e220615f7da4440dd3
Erlangga Yudi Pradana <erlanggayudipradana@gmail.com>
2025-10-16 16:23:24 +0700

    refactor(config): simplify config loading and hot reload logic

---
+++ src/config.rs
@@ -0,0 +1,61 @@
+use serde::Deserialize;
+use std::collections::HashMap;
+
+#[derive(Debug, Deserialize)]
+pub struct ScrapeConfigs {
+    #[serde(default = "ScrapeConfigs::default_scrape_timeout_seconds")]
+    pub scrape_timeout_seconds: u64,
+}
+
+impl ScrapeConfigs {
+    fn default_scrape_timeout_seconds() -> u64 {
+        10
+    }
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Config {
+    #[serde(default = "Config::default_scrape_configs")]
+    pub scrape_configs: ScrapeConfigs,
+    #[serde(default = "Config::default_basic_auth_users")]
+    pub basic_auth_users: HashMap<String, String>,
+    #[serde(default = "Config::default_log_config_file")]
+    pub log_config_file: String,
+}
+
+impl Config {
+    fn default_basic_auth_users() -> HashMap<String, String> {
+        HashMap::new()
+    }
+
+    fn default_scrape_configs() -> ScrapeConfigs {
+        ScrapeConfigs {
+            scrape_timeout_seconds: 10,
+        }
+    }
+
+    fn default_log_config_file() -> String {
+        String::from("log4rs.yaml")
+    }
+}
+
+impl Config {
+    pub fn get_config() -> Result<Self, Box<dyn std::error::Error + Send + Sync>> {
+        let config_file = std::env::var("CONFIG_FILE")
+            .map_err(|e| {
+                let error = format!("Environment variable `CONFIG_FILE` not found {}", e);
+                log::error!("{}", error);
+                error
+            })?;
+
+        let file = std::fs::File::open(config_file)
+            .map_err(|e| {
+                log::error!("failed to open config file: {}", e);
+                e
+            })?;
+
+        let config: Config = serde_yaml::from_reader(file)?;
+
+        Ok(config)
+    }
+}
